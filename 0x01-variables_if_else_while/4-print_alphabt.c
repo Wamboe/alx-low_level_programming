1b1380992a62a04908536c61c2a2e02fbc3253b2
Wamboe <sheila.karienye@gmail.com>
2023-04-24 13:56:09 -0700

    excluding q, e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints letter of the alphabet
+ *
+ * Return: always 0 for success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if c in ['q', 'e']
+		{
+			continue;	
+		}
+		else
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
