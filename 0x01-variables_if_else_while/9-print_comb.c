4a40aa4cea6f893ffa7066f1b15cfa71a269df07
Wamboe <sheila.karienye@gmail.com>
2023-04-24 15:21:49 -0700

    possible combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints letter of the alphabet
+ *
+ * Return: always 0 for success
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
