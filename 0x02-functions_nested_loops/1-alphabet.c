aef27cfed97d8e9ca234edcf6ac82a1f77812cea
Wamboe <sheila.karienye@gmail.com>
2023-04-25 10:26:45 -0700

    printing alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print lowercase alphabet
+ *
+ * Return: n/a
+ */
+void print_alphabet(void)
+{
+	int c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
