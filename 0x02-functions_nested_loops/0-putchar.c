b23b9c457412e657ff0ee5de624826a678f1167b
Wamboe <sheila.karienye@gmail.com>
2023-04-25 09:46:21 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - printing a string
+ *
+ * Return - 0 if successful
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
