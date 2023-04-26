ae4ffc429460684bc578b16ca7b14754043e32c0
Wamboe <sheila.karienye@gmail.com>
2023-04-25 18:11:58 -0700

    9 times tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/***/
+void times_table(void)
+{
+	int i, j, k, remainder, reverse = 0, l, count = 0;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+
+			if ((k / 10) != 0)
+			{
+				while (k != 0)
+				{
+					remainder = k % 10;
+					reverse = reverse * 10 + remainder;
+					k = k / 10;
+					count++;
+				}
+				
+				while (count != 0)
+				{
+					l = reverse % 10;
+					_putchar(l + '0');
+					reverse = reverse / 10;
+					count--;
+				}
+			}
+			else
+			{
+				_putchar(k + '0');
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
