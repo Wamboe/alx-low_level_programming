32c5cf830b42795ac6a735ad4deab1790d185047
Wamboe <sheila.karienye@gmail.com>
2023-04-27 19:55:20 -0700

    print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws line
+ * @n: size of line
+ * Return: n/a
+ */
+void print_triangle(int size)
+{
+	int i,j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (size == 1)
+	{
+		_putchar('#');
+		_putchar('\n');
+	}
+	else
+	{
+		while (size != 0)
+		{
+			
+			for(i = size; i > 0; --i)
+			{
+				_putchar(' ');
+			}
+			for(j = 0; j < size ; ++j)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			size--;
+		}
+	}
+}
