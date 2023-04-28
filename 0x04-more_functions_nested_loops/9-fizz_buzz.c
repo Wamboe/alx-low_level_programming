3cae97f6cdd84452fbf299693f757e52704566f2
Wamboe <sheila.karienye@gmail.com>
2023-04-27 18:31:31 -0700

    fizz buzz print

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if ((n % 3) == 0 && (n % 5) != 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 3) != 0 && (n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3) == 0 && (n % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	return (0);
+}
