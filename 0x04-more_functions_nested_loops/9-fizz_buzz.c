27a603708b0b51ea994d684eb59ae9bf809f35f1
Wamboe <sheila.karienye@gmail.com>
2023-04-27 18:39:05 -0700

    fizz buzz print new line fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,20 +14,23 @@ int main(void)
 	{
 		if ((n % 3) == 0 && (n % 5) != 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((n % 3) != 0 && (n % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if ((n % 3) == 0 && (n % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", n);
 		}
+
+		if (n != 100)
+			printf(" ");
 		n++;
 	}
 	printf("\n");
