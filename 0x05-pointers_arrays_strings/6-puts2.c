6dd34327a79c789fe0a5e1b04bc6aa87835f1806
Wamboe <sheila.karienye@gmail.com>
2023-05-02 16:43:45 -0700

    skipping one while printing a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,36 @@
 #include "main.h"
 
+
+#include <stdio.h>
+
 /**
- * puts2 - sets vars to 98;
+ * _strlen - get size of a string;
+ * @s: string being measured
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+        int i = 0, c = 0;
+
+        while (s[i] != '\0')
+        {
+                c++;
+                i++;
+        }
+        return (c);
+}
+
+/**
+ * puts2 - prints every other char in a string;
  * @str: pointer argument
  * Return: n/a
  */
 void puts2(char *str)
 {
+	int len = _strlen(str);
 	int i = 0;
 
-	while (*(str + i) != '\0')
+	while ((*(str + i) != '\0') && (i > len))
 	{
 		_putchar(*(str + i));
 		i = i + 2;
