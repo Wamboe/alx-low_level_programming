1e8ee1fb21fa2abf08c04e200dc034352c33f8b6
Wamboe <sheila.karienye@gmail.com>
2023-05-03 15:16:33 -0700

    copying to destination

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,19 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
+	while (dest[i] != '\0')
+	{
+		dest[i] = 0;
+	}
+	dest[i + 1] = 0;
+	i = 0;
+
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	i = i + 1;
-	dest[i] = '\0';
+	dest[i + 1] = '\0';
+
 	return (dest);
 }
