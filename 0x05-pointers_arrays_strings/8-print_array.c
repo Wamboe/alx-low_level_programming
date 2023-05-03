87e04a7f4a905b9de9663ffe68b02696a9e1fd56
Wamboe <sheila.karienye@gmail.com>
2023-05-03 12:28:48 -0700

    print array as per n

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints every other char in a string;
+ * @a: array to be printed
+ * @n: no. of array items to be printed
+ * Return: n/a
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n && n > 0)
+	{
+		printf("%d",*(a + i));
+		if (i + 1 != n)
+			printf(", ");
+		i++;
+	}
+	_putchar('\n');
+}
