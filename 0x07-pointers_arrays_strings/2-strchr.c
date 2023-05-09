995456453fd42722a2ad8e6e4c56d8a3b077aa93
Wamboe <sheila.karienye@gmail.com>
2023-05-08 22:17:01 -0700

    that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*(s) != '\0')
+	while (*(s) != c)
 	{
+/*
 		if (*(s) == c)
 		{
 			return (s);
 		}
+*/
 		s++;
 	}
-	return (NULL);
+	return (s);
 }
