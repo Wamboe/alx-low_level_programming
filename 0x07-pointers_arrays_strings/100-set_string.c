a2eda8e06e32632ca8a0b8895a1ed30d51f7e62c
Wamboe <sheila.karienye@gmail.com>
2023-05-09 04:11:29 -0700

    advanced correction attempt

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,10 @@
-/***/
+/**
+ * set_string - that sets the value of a pointer to a char
+ * @s: address to 1st string
+ * @to: string to be changed
+ * Return: n/a
+ */
+
 void set_string(char **s, char *to)
 {
 	*s = to;
