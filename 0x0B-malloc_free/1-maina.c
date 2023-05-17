81067965133c434054624d6dee702596a3b66790
Wamboe <sheila.karienye@gmail.com>
2023-05-16 17:31:43 -0700

    eturns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-maina.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strdup(char *str);
+
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s;
+
+	s = _strdup(NULL);
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	return (0);
+}
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * *_strdup - returns a pointer to a newly allocated space in memory
@@ -10,21 +11,18 @@
 char *_strdup(char *str)
 {
 	char *c;
-	int i = 0, len = strlen(str);
+	int len, i = 0;
 
 	if (str == NULL)
 		return (0);
 
+	len = strlen(str);
 	c = malloc(sizeof(char) * len);
-
-	if (c == NULL)
-		return (0);
-
 	while (i < len)
 	{
-
 		c[i] = str[i];
 		i++;
 	}
+
 	return (c);
 }
