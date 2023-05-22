eb9adbfb07f696cb7c45348a1141db8eeba08297
Wamboe <sheila.karienye@gmail.com>
2023-05-22 14:43:34 -0700

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - dog struct
+ * @name: member name
+ * @age: member age
+ * @owner: member owner
+ * Description: this is a dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
