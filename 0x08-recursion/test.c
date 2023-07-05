23f530ef687ba84909f7ea5ec495dd0340d97a5c
slwambui <slwambui@LTTECHMAC333.local>
2023-07-06 00:48:46 +0300

    recursively print reverse string

--- 0x08-recursion/test.c
+++ 0x08-recursion/test.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     int x = 1, y = 2, z[10], *ip;
     ip = &x;
+    char *c = "Sheila";
 
     printf("x:%d y:%d ", x,y);
     printf("*ip:%d ip:%p", *ip, ip);
@@ -23,4 +25,16 @@ int main()
 
     ip = &z[0];
     printf("*ip:%d ip:%p", *ip, ip);
+
+    printf("\n");
+
+    printf("%lu", strlen(c));
+
+    printf("\n");
+
+    printf("%lu", sizeof(c));
+
+    printf("\n");
+    printf("%d", 7/2);
+
 }
