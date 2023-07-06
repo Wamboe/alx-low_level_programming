fef455bd3de0805bd997957ecc2bcce0752a58f7
slwambui <slwambui@LTTECHMAC333.local>
2023-07-06 04:47:53 +0300

    squareroot improvement

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
+/**
+ * _sqrt_recursion - get squareroot of a number
+ * @n: number to get sqrt
+ * Return: -1 if not nutaral else sqrt
+*/
 int _sqrt_recursion(int n)
 {
-    double i = 0;
-    double j = n / 2;
+	double i = 0;
+	double j = n / 2;
 
-    while (j != i) 
-    {
-        i = j;
-        j = (n / i + i) / 2;
-    }
+	while (j != i)
+	{
+		i = j;
+		j = (n / i + i) / 2;
+	}
+	if (j == 0)
+	return (-1);
 
-    if (j == 0)
-    return (-1);
-
-    return (j);
+	return (j);
 }
