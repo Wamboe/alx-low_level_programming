a8930b9e05eb5ee14792051c0fe266b3885d0132
slwambui <slwambui@LTTECHMAC333.local>
2023-07-12 00:32:45 +0300

    multidimentional array betty corrections

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,30 +1,35 @@
 #include <stdlib.h>
 
-int **alloc_grid(int width, int height) {
-    int i, j, **ma;
+/**
+ * alloc_grid - allocates a multimentional array
+ * @width: width of the array
+ * @height: height of the array
+ * Return: array initialized in 0s
+*/
+int **alloc_grid(int width, int height)
+{
+	int i, j, **ma;
 
-    if (width < 1 || height < 1) {
-        return NULL;
-    }
+	if (width < 1 || height < 1)
+		return (NULL);
 
-    ma = (int **)malloc(height * sizeof(int *));
-  
-    if (ma == NULL) {
-        return NULL;
-    }
-  
-    for (i = 0; i < height; i++) {
-        ma[i] = (int *)calloc(width, sizeof(int));
-      
-        if (ma[i] == NULL) {
-            for (j = 0; j < i; j++) {
-                free(ma[j]);
-            }
-          
-            free(ma);
-            return NULL;
-        }
-    }
-  
-    return ma;
+	ma = (int **)malloc(height * sizeof(int *));
+
+	if (ma == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ma[i] = (int *)calloc(width, sizeof(int));
+		if (ma[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(ma[j]);
+			}
+			free(ma);
+			return (NULL);
+		}
+	}
+	return (ma);
 }
