862411efd62637f3f6bd2933b2fbf61adaa99c37
slwambui <slwambui@LTTECHMAC333.local>
2023-07-11 22:53:06 +0300

    strconcat correction check

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,10 +14,7 @@ char *str_concat(char *s1, char *s2)
 	char *c;
 	int i = 0;
 
-	if (s1[0] == '\0')
-		return (0);
-
-	if (s2[0] == '\0')
+	if (s1[0] == '\0' && s2[0] == '\0')
 		return (0);
 
 	c = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
