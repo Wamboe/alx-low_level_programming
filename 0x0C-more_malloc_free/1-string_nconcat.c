fb2915b42042ada424713c0328a11238c405b07e
slwambui <slwambui@LTTECHMAC333.local>
2023-07-14 00:58:45 +0300

    null checker

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * string_nconcat - concatenates two strings
  * @s1: string 1
@@ -15,21 +14,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i = 0, len1, len2, totalLen;
 
 	if (s1 == NULL)
-		len1 = 0;
-	else
-		len1 = strlen(s1);
-
+		s1 = "";
 	if (s2 == NULL)
-		len2 = 0;
-	else
-		len2 = strlen(s2);
+		s2 = "";
 
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 	if (n > sizeof(s2))
 		n = len2;
 
 	totalLen = len1 + n + 1;
-	str3 = malloc(totalLen * sizeof(char));
 
+	str3 = malloc(totalLen * sizeof(char));
 	if (str3 == NULL)
 		return (NULL);
 
@@ -46,6 +42,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2++;
 	}
 
-	str3[totalLen] = '\0';
+	str3[i] = '\0';
 	return (str3);
 }
