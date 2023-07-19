0fa4b35beeb7cbef373b082988a5741283163c51
slwambui <slwambui@LTTECHMAC333.local>
2023-07-19 20:50:03 +0300

    searching for an int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,28 @@
 #include "function_pointers.h"
 
+/**
+ * int_index - returns index fof int being searched for
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: fxn to apply to array
+ * Return: index of int or -1 if index not found
+*/
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int x = 0, rslt = 0;
+	int x = 0, rslt = 0;
 
-    if (size <= 0)
-        return (-1);
-    
+	if (size <= 0)
+		return (-1);
 
-    for ( ; x < size; x++)
-    {
-        rslt =cmp(array[x]);
-        if (rslt == 1)
-            break;
-    }
+	for ( ; x < size; x++)
+	{
+		rslt = cmp(array[x]);
+		if (rslt == 1)
+			break;
+	}
 
-    if (x == size && rslt == 0)
-    {
-        return(-1);
-    }
+	if (x == size && rslt == 0)
+		return (-1);
 
-    return (x);  
+	return (x);
 }
