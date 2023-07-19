7e479d88345c41134b579a0e1c07f0e6ab9a3ac4
slwambui <slwambui@LTTECHMAC333.local>
2023-07-19 20:13:05 +0300

    iterates over array and applies specific fxn

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - itarates over a given array
+ * @array: array to be iterated
+ * @size: size of array
+ * @action: function to be applied on array
+ * Return: n/a
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int x = 0;
+
+	for (; x < size; x++)
+		action(array[x]);
+}
