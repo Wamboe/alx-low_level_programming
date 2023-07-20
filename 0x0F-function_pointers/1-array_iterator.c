72e0c70322d75c2f57fe656791c5c85732c855f5
slwambui <slwambui@LTTECHMAC333.local>
2023-07-21 02:36:33 +0300

    fix q1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x = 0;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (; x < size; x++)
 		action(array[x]);
 }
